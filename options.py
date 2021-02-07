1561242efa082dcb82d6e202477957b71c429d3b
dingkang <wangdk93@outlook.com>
2021-02-07 18:18:55 -0500

    Add results

--- options.py
+++ options.py
@@ -10,7 +10,7 @@ class Options:
         self.parser.add_argument('--lr_start', '-ls', type=float, default=5e-6)
         self.parser.add_argument('--gpu', '-g', type=int, default=0)
         self.parser.add_argument('--lr_decay', '-ld', type=float, default=0.95)
-        self.parser.add_argument('--epoch_max', '-em', type=int, default=500) # please stop training mannully 
+        self.parser.add_argument('--epoch_max', '-em', type=int, default=200) # please stop training mannully 
         self.parser.add_argument('--epoch_from', '-ef', type=int, default=0) 
         self.parser.add_argument('--num_workers', '-j', type=int, default=8)
         self.parser.add_argument('--n_class', '-nc', type=int, default=9)
